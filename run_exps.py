7d50f3961f3e2903ac86e41ff1213b79ba17abb0
nevo <nevoagmon@gmail.com>
2018-12-18 19:26:17 +0200

    drouput implemented

--- run_exps.py
+++ run_exps.py
@@ -2,4 +2,7 @@ from hw2 import experiments
 
 for K in [32, 64]:
     for L in [2, 4, 8, 16]:
-        experiments.run_experiment(f'exp1_1_K{K}_L{L}', layers_per_block=L, filters_per_layer=[K])
+        try:
+            experiments.run_experiment(f'exp1_1_K{K}_L{L}', layers_per_block=L, filters_per_layer=[K], epochs=30)
+        except:
+            pass
